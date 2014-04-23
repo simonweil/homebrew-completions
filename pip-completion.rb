b05dc38fc9a9c96509b17cdc0140168c76ef8977
Simon Weil <simon.weil@naturalint.com>
2014-04-23 12:28:55 +0300

    fixed sha1

--- pip-completion.rb
+++ pip-completion.rb
@@ -3,7 +3,7 @@ require 'formula'
 class PipCompletion < GithubGistFormula
   url 'https://raw.githubusercontent.com/ekalinin/pip-bash-completion/d62c37ccdf46b937784ca5072de79558f786a3c3/pip'
   homepage 'https://github.com/ekalinin/pip-bash-completion'
-  sha1 '44793b16421386b754e476a0c2a15107cf61c253'
+  sha1 '0f738ac9f429e59b9c2f2ba769fe8ad38f3e4597'
 
   def install
     (prefix+'etc/bash_completion.d').install Dir['*']
