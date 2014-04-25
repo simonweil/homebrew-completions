28d026372d0857cf187894f0c259e331557b3014
Simon Weil <simon.weil@naturalint.com>
2014-04-25 11:38:39 +0300

    added sha1

--- bundler-completion.rb
+++ bundler-completion.rb
@@ -3,7 +3,7 @@ require 'formula'
 class BundlerCompletion < GithubGistFormula
   url 'https://raw.githubusercontent.com/mernen/completion-ruby/790a467456851fb563a5bbd26628465febbec2cc/completion-bundle'
   homepage 'https://github.com/mernen/completion-ruby'
-  sha1 '0f738ac9f429e59b9c2f2ba769fe8ad38f3e4597'
+  sha1 'edf3b701d388beb8065a369b5aa6673e272c880e'
 
   def install
     (prefix+'etc/bash_completion.d').install Dir['*']
