b7f0b03191d8959b0f3799e779720756676409b3
Simon Weil <simon.weil@naturalint.com>
2014-04-25 11:49:27 +0300

    added sha1

--- gem-completion.rb
+++ gem-completion.rb
@@ -3,7 +3,7 @@ require 'formula'
 class GemCompletion < GithubGistFormula
   url 'https://raw.githubusercontent.com/mernen/completion-ruby/790a467456851fb563a5bbd26628465febbec2cc/completion-gem'
   homepage 'https://github.com/mernen/completion-ruby'
-  sha1 '0f738ac9f429e59b9c2f2ba769fe8ad38f3e4597'
+  sha1 '2806f08be0f9ba2ece384097055c9cec0b74e06d'
 
   def install
     (prefix+'etc/bash_completion.d').install Dir['*']
--- ruby-completion.rb
+++ ruby-completion.rb
@@ -3,7 +3,7 @@ require 'formula'
 class RubyCompletion < GithubGistFormula
   url 'https://raw.githubusercontent.com/mernen/completion-ruby/790a467456851fb563a5bbd26628465febbec2cc/completion-ruby'
   homepage 'https://github.com/mernen/completion-ruby'
-  sha1 '0f738ac9f429e59b9c2f2ba769fe8ad38f3e4597'
+  sha1 '25ec4eb61601f3e17286503e270ad44256938255'
 
   def install
     (prefix+'etc/bash_completion.d').install Dir['*']
