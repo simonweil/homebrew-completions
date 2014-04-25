dce1fbd9827ed5872d33d5c6f3a53680c16376be
Simon Weil <simon.weil@naturalint.com>
2014-04-25 11:46:51 +0300

    added gem and ruby completions

---
+++ gem-completion.rb
@@ -0,0 +1,11 @@
+require 'formula'
+
+class GemCompletion < GithubGistFormula
+  url 'https://raw.githubusercontent.com/mernen/completion-ruby/790a467456851fb563a5bbd26628465febbec2cc/completion-gem'
+  homepage 'https://github.com/mernen/completion-ruby'
+  sha1 '0f738ac9f429e59b9c2f2ba769fe8ad38f3e4597'
+
+  def install
+    (prefix+'etc/bash_completion.d').install Dir['*']
+  end
+end
